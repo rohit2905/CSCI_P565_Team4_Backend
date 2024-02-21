9168aabe3e40a503b12a806d5fa452a1b9ab5466
rohit2905 <krohitgoud@gmail.com>
2024-02-20 22:37:34 -0500

    backend dockerization

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.get('/trigger', (req, res) => {
     res.send('Backend Service at render Re-triggered!!!');
 }); 
 
-
+console.log(process.env.MONGO_URI);
 // db
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
