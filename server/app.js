e0338a5a18109a4eb82c061aa6ea5f326352f68a
rohit2905 <krohitgoud@gmail.com>
2024-03-15 01:56:41 -0400

    jest

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 const path = require('path');
-
 // server
 const app = express();
 
@@ -49,26 +48,5 @@ app.use("/", messageRoutes);
 const serviceRoutes = require("./routes/serviceRoutes");
 app.use("/", serviceRoutes);
 
-const port = process.env.PORT || 8080;
-const server = app.listen(port, () => {
-    const address = server.address();
-    console.log(`Backend Server is running on port ${port}`)
-});
 
-
-// for chat app
-const io = require("socket.io")(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: process.env.DEPLOY_URL,
-    },
-  });
-
-/*
-TODO: Chats socket connections: kirthivasan pending
-*/
-
-/*
-TODO: Searching and filtering: kirthivasan and shalini
-*/
 module.exports = app;
