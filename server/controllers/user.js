3cde5d9008c2bd88bc0db8ccc0fec73b9d2c2c59
rohit2905 <krohitgoud@gmail.com>
2024-04-07 22:37:55 -0400

    cookie bug solved

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -185,7 +185,6 @@ exports.login = async (req, res) => {
             });
 
             // persist the token as 'jwt' in cookie with an expiry date
-            const expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000); 
             res.cookie("jwt", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, secure: true, sameSite: "None" });
             if (user.passReset) {
 
