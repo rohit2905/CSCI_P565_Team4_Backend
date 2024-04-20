74a95b8c705dec0eeb93ab80d267fe5cb7dbec6b
Kirthivasan <kirthivasanp@gmail.com>
2024-04-20 13:08:25 -0400

    cron bug

--- server/app.js
+++ server/app.js
@@ -1,55 +1,62 @@
 // import modules
-const express = require('express');
-const {json, urlencoded} = express;
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const { json, urlencoded } = express;
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
-const path = require('path');
+const path = require("path");
 
 const passport = require("passport");
-const session = require('express-session');
+const session = require("express-session");
 const passportSetup = require("./passport");
 const authRoute = require("./routes/auth");
+const job = require("./cron.js").job;
 
 // server
 const app = express();
 
-/*
+job.start();
 
-TODO: CRON JOB for hitting the deployment url for every 30 min.
-Reason: free verson of Render goes down after sometime of inactivity
-*/
-
-app.get('/', (req, res) => {
-    res.send('Backend Service for DeliverWise Running');
+app.get("/", (req, res) => {
+  res.send("Backend Service for DeliverWise Running");
 });
 
 // Add express-session middleware
-app.use(session({
+app.use(
+  session({
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
-}));
+  })
+);
 
 // Initialize Passport and restore authentication state if available
 app.use(passport.initialize());
 app.use(passport.session());
 
 // db
-mongoose.connect(process.env.ENV === 'test' ?process.env.MONGO_URI_TEST:process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("Database Connectecd...")).catch((err) => console.log("Database Connection Error", err));
+mongoose
+  .connect(
+    process.env.ENV === "test"
+      ? process.env.MONGO_URI_TEST
+      : process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Database Connectecd..."))
+  .catch((err) => console.log("Database Connection Error", err));
 
 // middleware
 app.use(morgan("dev"));
-app.use(cors({ origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 app.use(json());
 app.use("/auth", authRoute);
-app.use(urlencoded({extended: false}));
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(expressValidator());
 app.set("trust proxy", 1);
@@ -67,9 +74,9 @@ app.use("/", messageRoutes);
 const serviceRoutes = require("./routes/serviceRoutes");
 app.use("/", serviceRoutes);
 const reviewRoutes = require("./routes/reviewRoutes");
-app.use("/", reviewRoutes)
+app.use("/", reviewRoutes);
 
 const authRoutes = require("./routes/auth");
-app.use("/", authRoutes)
+app.use("/", authRoutes);
 
 module.exports = app;
